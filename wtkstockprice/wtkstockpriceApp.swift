350b9f391c494d22d5fb2515f3ebc32e964aadea
cnwang <cnwang406@gmail.com>
2022-09-15 11:52:05 +0800

    Initial Commit

---
+++ wtkstockprice/wtkstockpriceApp.swift
@@ -0,0 +1,17 @@
+//
+//  wtkstockpriceApp.swift
+//  wtkstockprice
+//
+//  Created by Chun nan Wang on 2022/9/15.
+//
+
+import SwiftUI
+
+@main
+struct wtkstockpriceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
